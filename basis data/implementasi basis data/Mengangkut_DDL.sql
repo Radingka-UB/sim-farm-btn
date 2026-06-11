723b86db716b575d55a263ceb7c8acafbddf61e2
BuatNugas(Second) <radingkaarfian@student.ub.ac.id>
2026-06-11 18:44:35 +0700

    memperbaiki query foreign key

--- basis data/implementasi basis data/Mengangkut_DDL.sql
+++ basis data/implementasi basis data/Mengangkut_DDL.sql
@@ -13,6 +13,6 @@ CREATE TABLE MENGANGKUT
   id_shipping INT NOT NULL,
   id_customer INT NOT NULL,
   PRIMARY KEY (id_harvesting, id_kandang, id_shipping, id_customer),
-  FOREIGN KEY (id_harvesting, id_kandang) REFERENCES EGGHARVESTING(id_harvesting, ),
-  FOREIGN KEY (id_shipping, id_customer) REFERENCES EGGSHIPPING(id_shipping, )
+  FOREIGN KEY (id_harvesting, id_kandang) REFERENCES EGGHARVESTING(id_harvesting, id_kandang),
+  FOREIGN KEY (id_shipping, id_customer) REFERENCES EGGSHIPPING(id_shipping, id_customer)
 );
