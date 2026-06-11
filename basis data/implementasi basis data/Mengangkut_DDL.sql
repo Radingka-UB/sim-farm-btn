13577a66c4e3515be70758cb5adab299bab82c11
BuatNugas(Second) <radingkaarfian@student.ub.ac.id>
2026-06-11 18:02:52 +0700

    Menambahkan query database ke tiap file DDL

--- basis data/implementasi basis data/Mengangkut_DDL.sql
+++ basis data/implementasi basis data/Mengangkut_DDL.sql
@@ -1,3 +1,11 @@
+IF NOT EXISTS (SELECT name FROM sys.databases WHERE name = N'SIM_FARM_BTN')
+    CREATE DATABASE SIM_FARM_BTN;
+GO
+
+USE SIM_FARM_BTN;
+GO
+
+
 CREATE TABLE MENGANGKUT
 (
   id_harvesting INT NOT NULL,
@@ -5,6 +13,6 @@ CREATE TABLE MENGANGKUT
   id_shipping INT NOT NULL,
   id_customer INT NOT NULL,
   PRIMARY KEY (id_harvesting, id_kandang, id_shipping, id_customer),
-  FOREIGN KEY (id_harvesting, id_kandang) REFERENCES EGGHARVESTING(id_harvesting, id_kandang),
-  FOREIGN KEY (id_shipping, id_customer) REFERENCES EGGSHIPPING(id_shipping, id_customer)
+  FOREIGN KEY (id_harvesting, id_kandang) REFERENCES EGGHARVESTING(id_harvesting, ),
+  FOREIGN KEY (id_shipping, id_customer) REFERENCES EGGSHIPPING(id_shipping, )
 );
