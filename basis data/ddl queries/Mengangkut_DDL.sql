eae1b7bf23859c8cbeb1d23eb413b2dff3257548
BuatNugas(Second) <radingkaarfian@student.ub.ac.id>
2026-06-15 10:13:25 +0700

    mengubah ddl mengangkut ke 3nf yang benar
    Removed id_kandang and id_customer from the MENGANGKUT table. Updated primary and foreign key constraints accordingly.

--- basis data/ddl queries/Mengangkut_DDL.sql
+++ basis data/ddl queries/Mengangkut_DDL.sql
@@ -9,10 +9,8 @@ GO
 CREATE TABLE MENGANGKUT
 (
   id_harvesting INT NOT NULL,
-  id_kandang INT NOT NULL,
   id_shipping INT NOT NULL,
-  id_customer INT NOT NULL,
-  PRIMARY KEY (id_harvesting, id_kandang, id_shipping, id_customer),
-  FOREIGN KEY (id_harvesting, id_kandang) REFERENCES EGGHARVESTING(id_harvesting, id_kandang),
-  FOREIGN KEY (id_shipping, id_customer) REFERENCES EGGSHIPPING(id_shipping, id_customer)
+  PRIMARY KEY (id_harvesting, id_shipping),
+  FOREIGN KEY (id_harvesting) REFERENCES EGGHARVESTING(id_harvesting),
+  FOREIGN KEY (id_shipping) REFERENCES EGGSHIPPING(id_shipping)
 );
