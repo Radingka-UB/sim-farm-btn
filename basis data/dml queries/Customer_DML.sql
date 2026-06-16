42ee639685031eaba1375bc4d2933bbdc10b8165
Bilhaq Schrodinger <billibilhaq@gmail.com>
2026-06-16 19:12:57 +0700

    menghapus folder ddl, dml, model data yang salah

--- basis data/dml queries/Customer_DML.sql
+++
@@ -1,9 +0,0 @@
-USE SIM_FARM_BTN;
-GO
-DELETE FROM CUSTOMER;
-INSERT INTO CUSTOMER (id_customer, nama_customer, jenis_customer) VALUES
-(1, 'Supermarket Indomakmur', 'Supermarket'),
-(2, 'Agen Telur Berkah Jaya', 'Agen/Pasar'),
-(3, 'Pabrik Roti & Kue Lezat', 'Pabrik Industri'),
-(4, 'Pasar Induk Kramat Jati - Toko Amanah', 'Agen/Pasar'),
-(5, 'Giant Distributor Egg', 'Distributor Besar');
--- basis data/dml queries/Mortality_DML.sql
+++
@@ -1,9 +0,0 @@
-USE SIM_FARM_BTN;
-GO
-DELETE FROM MORTALITY;
-
-INSERT INTO MORTALITY (id_mortality, tanggal, penyebab, jumlah_mati, id_kandang) VALUES
-(801, '2026-06-01', 'Kanibalisme', 2, 1001),
-(802, '2026-06-01', 'Tidak Diketahui', 1, 1002),
-(803, '2026-06-02', 'Sakit', 3, 1004),
-(804, '2026-06-03', 'Sakit', 160, 1003);
